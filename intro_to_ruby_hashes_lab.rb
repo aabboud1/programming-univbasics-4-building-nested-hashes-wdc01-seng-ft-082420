cb3fe6df70b8124d72db1bb1b12afb713d93f8c0
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-09 15:19:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  base_hash = {
+  base_hash = {:railroads => {
     :pieces => 4,
     :rent_in_dollars => {
       one_piece_owned: 25, 
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
       :shortline_railroad => {}
     }
   }
-
+}
 end
 
 def monopoly_with_fourth_tier
